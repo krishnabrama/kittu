fe32d90a836bf4d6c843e0f4a50fa50d86742f8e
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-21 16:38:03 +0530

    Add files via upload

---
+++ EvenandOddSumRiverse.java
@@ -0,0 +1,25 @@
+package bynary.operators;
+
+public class EvenandOddSumRiverse {
+
+	public static void main(String[] args) {
+		/*
+		 
+		 * 1 2 3 4 5
+		 * 1 2 3 4
+		 * 1 2 3
+		 * 1 2 
+		 * 1 	
+		 */
+			
+			for(int r= 1;r<=5;r++) {
+				for(int c = 1 ;c<=r;c++) {
+					System.out.print(c + " ");
+				}
+				System.out.println();
+			}
+
+
+	}
+
+}
