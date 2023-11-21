b4ab44d94c65b822f8888163e1bea709e8763ae6
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-21 15:37:30 +0530

    Add files via upload

---
+++ Ss.java
@@ -0,0 +1,33 @@
+package bynary.operators;
+
+public class Ss {
+
+	public static void main(String[] args) {
+		 
+		int p=20;
+		do {
+			System.out.println("Welcome....."+p);
+			p++;/// 11
+		}while(p <=5);// 11 <=5--- false
+		
+		// while
+		int h=20;
+		while(h<=5) {
+			System.out.println("Hello...."+h);
+			h++;
+		}
+		
+		for(int k=20;k<=5;k++) {
+			System.out.println("Java....."+k);
+		}
+		
+		
+		
+		System.out.println("Program Ends");
+	}
+
+
+
+	}
+
+
