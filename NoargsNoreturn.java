fc1f0cb1b855fcf438a5dc4f2cce39ae2885ef2e
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-23 15:57:09 +0530

    Add files via upload

---
+++ NoargsNoreturn.java
@@ -0,0 +1,25 @@
+package bynary.operators;
+
+public class NoargsNoreturn {
+
+	
+	
+		
+		// methods-- 4 types
+		// no args ...no return
+		
+		static void design(){
+			System.out.println("****");
+			System.out.println("--------");
+		}
+		public static void main (String[] args) {
+			
+			design();
+			System.out.println("Hello");
+			design();
+			System.out.println("Welcome");
+			design();
+		
+	}
+
+}
