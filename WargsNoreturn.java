8b922b7fd6be36ffe779bfa8e3f3c20aa92b459b
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-23 16:00:37 +0530

    Add files via upload

---
+++ WargsNoreturn.java
@@ -0,0 +1,30 @@
+package bynary.operators;
+
+public class WargsNoreturn {
+
+	
+		// methods-- 4 types
+		//  args ...no return
+		
+		static void design(char c1,char c2){
+			for(int i=1;i<10;i++) {
+				System.out.print(c1);	
+			}
+			System.out.println();
+			for(int i=1;i<10;i++) {
+				System.out.print(c2);	
+			}
+			System.out.println();
+			
+			
+		}
+		public static void main(String[] args) {
+			
+			design('.','!');
+			System.out.println("Hello");
+			design(',','$');
+			System.out.println("Welcome");
+			design('@','%');
+	}
+
+}
