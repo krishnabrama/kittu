421e19b2990d5401fa948f45555e8524703fd774
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-24 16:57:09 +0530

    Add files via upload

---
+++ Primeorntype3.java
@@ -0,0 +1,31 @@
+package bynary.operators;
+
+public class Primeorntype3 {
+
+
+		static boolean toFindNumPrimeOrNot(int n){
+			int i=2;
+			boolean res = true;
+			
+			for(;(i<n/2) && (n % i != 0);i++);
+			
+			if(i < n/2) res = false;
+			return res;
+		}
+		public static void main(String[] args) {
+		
+			int num =2;
+			System.out.println("Result----->"+toFindNumPrimeOrNot(num));
+			
+		
+		
+		
+		
+		}
+
+		
+		
+	
+	}
+
+
