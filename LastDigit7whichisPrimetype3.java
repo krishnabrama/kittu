fbb680993e896e594bd70c2adc6457fef00d29b6
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-24 19:48:24 +0530

    Add files via upload

---
+++ LastDigit7whichisPrimetype3.java
@@ -0,0 +1,38 @@
+package bynary.operators;
+
+public class LastDigit7whichisPrimetype3 {
+	static boolean toFindNumPrimeOrNot(int n){
+		int i=2;
+		boolean res = true;
+		
+		for(;(i<n/2) && (n % i != 0);i++);
+		
+		if(i < n/2) res = false;
+		return res;
+	}
+	public static void main(String[] args) {
+	
+		
+		// last digit should be 7 which is prime number
+		
+		for(int i=200,count =0,m=0,f=0,l=0;i>=100;i--) {
+			if ( toFindNumPrimeOrNot(i) == true) {
+				l= i % 10;
+				f = i / 100;
+				m = (i / 10) % 10;
+				if(m < (f+l))
+				System.out.print(i+" ");
+				
+				
+			}
+		}
+	
+
+	}
+
+	
+	
+}
+	
+
+
