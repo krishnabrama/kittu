844c6698d8b08a8d251cf8b943919a43df70d80c
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-27 16:08:55 +0530

    Add files via upload

---
+++ Index.java
@@ -0,0 +1,20 @@
+package bynary.operators;
+
+public class Index {
+
+	public static void main(String[] args) {
+		String  str = "Hello Welcome";
+		int index = str.indexOf("W");
+		
+		String newStr = str.substring(index, str.length());
+		
+		System.out.println(newStr);
+		
+		
+		
+	}
+
+
+	}
+
+
